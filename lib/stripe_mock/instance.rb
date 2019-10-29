01b705464db2c74a9098803bd0d012ea707a7c55
Klaus Zanders <klaus.zanders@ioki.com>
2019-10-29 13:17:18 +0100

    Add OAuth and Dashboard Links (Express Setup Workflow)

--- lib/stripe_mock/instance.rb
+++ lib/stripe_mock/instance.rb
@@ -39,6 +39,7 @@ module StripeMock
     include StripeMock::RequestHandlers::Events
     include StripeMock::RequestHandlers::Invoices
     include StripeMock::RequestHandlers::InvoiceItems
+    include StripeMock::RequestHandlers::OAuth
     include StripeMock::RequestHandlers::Orders
     include StripeMock::RequestHandlers::Plans
     include StripeMock::RequestHandlers::Products

7ecc5b10fee706a45f0433770d08e45eccf306d6
Klaus Zanders <klaus.zanders@ioki.com>
2020-06-29 11:02:32 +0200

    Implement API Changes for Stripe::Account object
    https://stripe.com/docs/connect/required-updates/accounts-arguments

--- lib/stripe_mock/data.rb
+++ lib/stripe_mock/data.rb
@@ -21,6 +21,9 @@ module StripeMock
         country: "US",
         object: "account",
         business_name: "Stripe.com",
+        business_profile: {
+          name: 'Stripe.com'
+        },
         business_url: nil,
         support_phone: nil,
         managed: false,
@@ -34,6 +37,13 @@ module StripeMock
           data: [
 
           ]
+        },
+        requirements: {
+          disabled_reason: nil,
+          eventually_due: [],
+          currently_due: [],
+          past_due: [],
+          current_deadline: nil
         },
         verification: {
           fields_needed: [],
@@ -57,6 +67,18 @@ module StripeMock
             has_more: false,
             total_count: 0,
             url: "/v1/accounts/#{id}/external_accounts"
+        },
+        company: {
+          name: 'Stripe.com',
+          tax_id: nil,
+          address: {
+            line1: nil,
+            line2: nil,
+            city: nil,
+            state: nil,
+            postal_code: nil,
+            country: "US"
+          },
         },
         legal_entity: {
           type: nil,
